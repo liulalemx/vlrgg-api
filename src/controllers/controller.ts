4c6f39ac9e176578306b7a008c6e0ba394278c3d
Liul Alemayehu <liulalemx@gmail.com>
2023-02-23 22:20:53 +0300

    Make controller async

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -3,28 +3,28 @@ import { Response, Request } from "express";
 // @desc   GET teams
 // @route  GET /api/teams
 // @access Public
-const getTeams = (req: Request, res: Response) => {
+const getTeams = async (req: Request, res: Response) => {
   res.status(200).json({ msg: "teams" });
 };
 
 // @desc   GET players
 // @route  GET /api/players
 // @access Public
-const getPlayers = (req: Request, res: Response) => {
+const getPlayers = async (req: Request, res: Response) => {
   res.status(200).json({ msg: "players" });
 };
 
 // @desc   GET events
 // @route  GET /api/events
 // @access Public
-const getEvents = (req: Request, res: Response) => {
+const getEvents = async (req: Request, res: Response) => {
   res.status(200).json({ msg: "events" });
 };
 
 // @desc   GET event
 // @route  GET /api/events/:name
 // @access Public
-const getEvent = (req: Request, res: Response) => {
+const getEvent = async (req: Request, res: Response) => {
   res.status(200).json({ msg: `event ${req.params.name}` });
 };
 
